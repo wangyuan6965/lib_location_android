ab7aade57484afb852ce45c252d2e076c7bf8ebb
wangyuan <292449150@qq.com>
2023-01-14 12:52:06 +0800

    使用GMS获取地理位置

--- liblocation/src/main/java/priv/wangyuan/location/liblocation/GmsLocationApi.kt
+++ liblocation/src/main/java/priv/wangyuan/location/liblocation/GmsLocationApi.kt
@@ -3,21 +3,30 @@ package priv.wangyuan.location.liblocation
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Looper
 import androidx.core.app.ActivityCompat
 import com.blankj.utilcode.util.Utils
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import priv.wangyuan.location.liblocation.listener.GpsLocationListener
-import java.util.WeakHashMap
+import java.util.*
 
-class LocationApi private constructor() {
+class GmsLocationApi private constructor() {
 
     private val mListeners = WeakHashMap<GpsLocationListener, String>()
 
     private val mLocationManager by lazy {
         Utils.getApp().getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
+
+    private val mLocationRequest by lazy {
+        LocationRequest.create().setInterval(5000).setFastestInterval(5000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+    }
+    private val mLocationProvider by lazy { LocationServices.getFusedLocationProviderClient(Utils.getApp()) }
 
     private fun checkPermission(): Boolean {
         val context = Utils.getApp()
@@ -41,22 +50,20 @@ class LocationApi private constructor() {
         }
     }
 
-    private val mLocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            mListeners.keys.forEach { l ->
-                l.onLocationChanged(location)
-            }
-        }
+    private val mLocationCallback = object : LocationCallback() {
 
-        override fun onProviderDisabled(provider: String) {
-            super.onProviderDisabled(provider)
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
             mListeners.keys.forEach { l ->
-                l.onLocationProviderDisabled()
+                val locations = result.locations
+                locations.takeIf { it.isNotEmpty() }?.also {
+                    l.onLocationChanged(it[0])
+                }
             }
         }
     }
 
-    fun requestLocationUpdates(l : GpsLocationListener) {
+    fun requestLocationUpdates(l: GpsLocationListener) {
         mListeners[l] = ""
         if (!checkPermission()) {
             mListeners.keys.forEach {
@@ -65,11 +72,16 @@ class LocationApi private constructor() {
             return
         }
         getEnabledProvider()?.also {
-            val location = mLocationManager.getLastKnownLocation(it)
-            mListeners.keys.forEach { l ->
-                l.onLastLocationChanged(location)
+            mLocationProvider.lastLocation.addOnSuccessListener {
+                mListeners.keys.forEach { l ->
+                    l.onLastLocationChanged(it)
+                }
             }
-            mLocationManager.requestLocationUpdates(it, 5000, 10f, mLocationListener)
+            mLocationProvider.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                Looper.getMainLooper()
+            )
         } ?: run {
             mListeners.keys.forEach {
                 it.onLocationProviderDisabled()
@@ -77,18 +89,18 @@ class LocationApi private constructor() {
         }
     }
 
-    fun removeLocationUpdates(l : GpsLocationListener) {
+    fun removeLocationUpdates(l: GpsLocationListener) {
         mListeners.remove(l)
-        mLocationManager.removeUpdates(mLocationListener)
+        mLocationProvider.removeLocationUpdates(mLocationCallback)
     }
 
     private object Holder {
-        val INSTANCE = LocationApi()
+        val INSTANCE = GmsLocationApi()
     }
 
     companion object {
         @JvmStatic
-        fun getInstance() : LocationApi {
+        fun getInstance(): GmsLocationApi {
             return Holder.INSTANCE
         }
     }
