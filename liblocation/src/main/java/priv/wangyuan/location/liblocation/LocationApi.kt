f7458592120eb8b482ff8cb293eddb82be4d4572
wangyuan <292449150@qq.com>
2023-01-14 13:18:26 +0800

    代码合并

--- liblocation/src/main/java/priv/wangyuan/location/liblocation/LocationApi.kt
+++ liblocation/src/main/java/priv/wangyuan/location/liblocation/LocationApi.kt
@@ -9,7 +9,7 @@ import android.location.LocationManager
 import androidx.core.app.ActivityCompat
 import com.blankj.utilcode.util.Utils
 import priv.wangyuan.location.liblocation.listener.GpsLocationListener
-import java.util.WeakHashMap
+import java.util.*
 
 class LocationApi private constructor() {
 
@@ -81,6 +81,58 @@ class LocationApi private constructor() {
         mListeners.remove(l)
         mLocationManager.removeUpdates(mLocationListener)
     }
+
+    /*GMS获取start*/
+    /*private val mLocationRequest by lazy {
+        LocationRequest.create().setInterval(5000).setFastestInterval(5000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+    }
+    private val mLocationProvider by lazy { LocationServices.getFusedLocationProviderClient(Utils.getApp()) }
+
+    private val mLocationCallback = object : LocationCallback() {
+
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            mListeners.keys.forEach { l ->
+                val locations = result.locations
+                locations.takeIf { it.isNotEmpty() }?.also {
+                    l.onLocationChanged(it[0])
+                }
+            }
+        }
+    }
+
+    fun requestGmsLocationUpdates(l: GpsLocationListener) {
+        mListeners[l] = ""
+        if (!checkPermission()) {
+            mListeners.keys.forEach {
+                it.onLocationPermissionDenied()
+            }
+            return
+        }
+        getEnabledProvider()?.also {
+            mLocationProvider.lastLocation.addOnSuccessListener {
+                mListeners.keys.forEach { l ->
+                    l.onLastLocationChanged(it)
+                }
+            }
+            mLocationProvider.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                Looper.getMainLooper()
+            )
+        } ?: run {
+            mListeners.keys.forEach {
+                it.onLocationProviderDisabled()
+            }
+        }
+    }
+
+    fun removeGmsLocationUpdates(l: GpsLocationListener) {
+        mListeners.remove(l)
+        mLocationProvider.removeLocationUpdates(mLocationCallback)
+    }*/
+    /*GMS获取end*/
 
     private object Holder {
         val INSTANCE = LocationApi()
